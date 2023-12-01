016078daea2523067ecec597820984bd4f5b3b6b
Karolina Kozubik <k4aro0@gmail.com>
2023-12-01 21:05:47 +0100

    add template file

--- data.sh
+++ data.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 month=$(date +"%m")
+
 if [ $month -ne 12 ]; then
     echo "It's not December, AoC hasn't started yet."
     exit
 fi
 
 hour=$(date +"%H")
+
 if [ $hour -lt 6 ]; then
     echo "It's too early, today's challenge hasn't been posted yet."
     exit
@@ -31,34 +33,12 @@ fi
 
 if [ ! -s "$day_dir/input.txt" ] && [ ! -s "$day_dir/example.txt" ]; then
     pipenv run aocd $year $day > "$day_dir/input.txt"
+    pipenv run aocd $year $day --example > "$day_dir/example.txt"
 fi
 
-pipenv run aocd $year $day --example > "$day_dir/example.txt"
-
 if [ ! -s "$day_dir/part1.py" ] && [ ! -s "$day_dir/part2.py" ]; then
-    echo "from aocd import submit
-
-def solve(data):
-    pass
-
-filecontent = ''
-with open('input.txt') as file:
-    filecontent = file.read()
-
-result = solve(filecontent)
-#submit(result, part='a', day=$day, year=$year)" > "$day_dir/part1.py"
-
-    echo "from aocd import submit
-
-def solve(data):
-        pass
-
-filecontent = ''
-with open('input.txt') as file:
-    filecontent = file.read()
-
-result = solve(filecontent)
-#submit(result, part='b', day=$day, year=$year)" > "$day_dir/part2.py"
+    printf "$(cat ".templates/template.py.txt")" $year $day 1 "$day_dir/input.txt" > "$day_dir/part1.py"
+    printf "$(cat ".templates/template.py.txt")" $year $day 2 "$day_dir/input.txt" > "$day_dir/part2.py"
 fi
 
 pipenv shell
